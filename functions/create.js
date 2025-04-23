f61f7d5879f63c65a9efee6ba043d2d16a204a4a
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-04-23 03:58:33 -0500

    Update create.js

--- functions/create.js
+++ functions/create.js
@@ -8,17 +8,20 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use('/',(req,res)=>{var connect = mysql.createConnection('mysql://avnadmin:AVNS_om8uYVTBL50tPl05R_4@mysql-1e9f0822-jpbreaux225-37e4.h.aivencloud.com:25589/defaultdb?ssl-mode=REQUIRED')
 connect.query(`Create Table Customer(
-PRIMARY KEY id varchar(255),
+ID int NOT NULL,
 username varchar(255),
 pasword varchar(255)
 date_of_creation varchar(255),
 medical_conditions varchar(255),
-prescription_drugs varchar(255)
+prescription_drugs varchar(255),
+PRIMARY KEY(ID)
 )`,(err,results)=>{connect.query(`Create Table Records(
-PRIMARY KEY id varchar(255),
+ID int NOT NULL,
 username varchar(255),
 date varchar(255),
-medical_info varchar(255)
+medical_info varchar(255),
+PRIMARY KEY(ID)
+
 )`)})
 
 
