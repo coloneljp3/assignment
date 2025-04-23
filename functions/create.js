b053614cf5a25ab8241b54a2da459832fd736fcb
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-04-23 04:56:53 -0500

    Update create.js

--- functions/create.js
+++ functions/create.js
@@ -9,7 +9,6 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.urlencoded({extended:true}))
 app.use('/',(req,res)=>{var connect = mysql.createConnection('mysql://avnadmin:AVNS_om8uYVTBL50tPl05R_4@mysql-1e9f0822-jpbreaux225-37e4.h.aivencloud.com:25589/defaultdb?ssl-mode=REQUIRED')
  connect.query(`USE defaultdb;Create Table Customer(
- PRIMARY KEY id varchar(255),
  ID int NOT NULL,
  username varchar(255),
  pasword varchar(255)
@@ -19,7 +18,6 @@ app.use('/',(req,res)=>{var connect = mysql.createConnection('mysql://avnadmin:A
  prescription_drugs varchar(255),
  PRIMARY KEY(ID)
  )`,(err,results)=>{connect.query(`USE defaultdb;Create Table Records(
- PRIMARY KEY id varchar(255),
  ID int NOT NULL,
  username varchar(255),
  date varchar(255),
