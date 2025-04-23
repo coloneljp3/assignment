7232f873622721145d1785414b76e2515a361006
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-04-23 04:47:36 -0500

    Update create.js

--- functions/create.js
+++ functions/create.js
@@ -7,7 +7,7 @@ var serverless = require('serverless-http')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use('/',(req,res)=>{var connect = mysql.createConnection('mysql://avnadmin:AVNS_om8uYVTBL50tPl05R_4@mysql-1e9f0822-jpbreaux225-37e4.h.aivencloud.com:25589/defaultdb?ssl-mode=REQUIRED');
-connect.query(`Select * from Customer`,(err,results)=>{res.send(results)})
+connect.query(`Select * from Customer`,(err,results)=>{res.send(err)})
 
 
 })
