e8e552ebc1e1c298cf081f573788e723109d78ce
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-04-25 01:35:43 -0500

    Update medical-forms.js

--- functions/medical-forms.js
+++ functions/medical-forms.js
@@ -13,6 +13,8 @@ app.use('/',(req,res)=>{
 var username = req.body.username
 var pasword = req.body.pasword
 var date = new Date()
+var medical_data = {};
+var prescription_data = {}
 var prescription_drugs =[]
 var medical_conditions = []
 var connect = mysql.createConnection('mysql://avnadmin:AVNS_om8uYVTBL50tPl05R_4@mysql-1e9f0822-jpbreaux225-37e4.h.aivencloud.com:25589/defaultdb?ssl-mode=REQUIRED')
