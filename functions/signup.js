287b1865a77efc2b84a67da8df152a223a42a481
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-04-25 06:21:55 -0500

    Update signup.js

--- functions/signup.js
+++ functions/signup.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true}));
 
 
 
-app.use('/',(req,res)=>{
+app.post('/',(req,res)=>{
 var customer = req.body
 var username = customer.username
 var pasword = customer.pasword
