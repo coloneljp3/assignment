c74ad0f2994c9794169fb7e8aea26b87b36ff279
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-05-01 02:55:14 -0500

    Update medical-forms.js

--- functions/medical-forms.js
+++ functions/medical-forms.js
@@ -7,8 +7,8 @@ const serverless = require('serverless-http')
 const bodyParser = require('body-parser')
 app.use(express.json())
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
-router.get('/',(req,res)=>{res.send("med")})
+app.use(bodyParser.urlencoded({extended:true}))
+router.get('/',(req,res)=>{res.send(`med`)})
 router.post('/',(req,res)=>{
 var body = req.body
 var path_of_form = req.body.path_of_form
