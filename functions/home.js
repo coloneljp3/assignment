78b26de2572936556eb5b8db8220f412c4d46bd0
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-05-02 11:37:34 -0500

    Update home.js

--- functions/home.js
+++ functions/home.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.use('/',(req,res)=>{
-res.setHeader('content-type','application/json')
+res.set('content-type','application/json')
 var username = req.body.username
 var pasword = req.body.pasword
 res.send(`<html><head>
