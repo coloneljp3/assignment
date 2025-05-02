6437490b25a604ced86afc9ffe7f265416de7be0
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-05-02 11:20:32 -0500

    Update medical-forms.js

--- functions/medical-forms.js
+++ functions/medical-forms.js
@@ -14,7 +14,6 @@ router.post('/',(req,res)=>{
 var body = req.body
 var path_of_form = req.body.path_of_form
 var username = body.username
-res.send(body)
 var pasword = req.body.pasword
 var date = new Date()
 var medical_data = {};
