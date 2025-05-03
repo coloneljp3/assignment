03291c9fb5f37622d6874b88f29dfc5cbda853fd
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-05-03 15:16:32 -0500

    Create extract.js

---
+++ functions/extract.js
@@ -0,0 +1,17 @@
+'use strict'
+const express = require('express')
+const mysql = require('mysql2')
+const app = express()
+const router = express.Router()
+const serverless = require('serverless-http')
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+router.post('/',(req,res)=>{res.send(`
+
+<form method = "POST" action = "/.netlify/functions/extractdb"><input name = "query" placeholder = "type query"/><button type = "submit"></button></form>
+
+`)})
+app.use('/.netlify/functions/extract',router)
+module.exports.handler = serverless(app)
