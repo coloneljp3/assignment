6b3889126a1d57c015554037bd3fa62d80841995
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-05-05 06:16:43 -0500

    Update spare.js

--- functions/spare.js
+++ functions/spare.js
@@ -9,19 +9,17 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 router.get("/",(req,res)=>{
 var conn = mysql.createConnection('mysql://avnadmin:AVNS_om8uYVTBL50tPl05R_4@mysql-1e9f0822-jpbreaux225-37e4.h.aivencloud.com:25589/defaultdb?ssl-mode=REQUIRED')
-conn.query(`Create Table Customers(
+conn.query(`Create Table Records(
 id int(16) auto_increment,
 username varchar(255),
-pasword varchar(255),
-medical_conditions TEXT NOT NULL,
-prescription_drugs TEXT NOT NULL,
+daily_dosage varchar(255),
 PRIMARY KEY (id)
 )
 `,(err,result)=>{
 
 
 
-conn.query(`ALTER TABLE Customers AUTO_INCREMENT = 1`,(err,result)=>{res.send(err,result)})
+conn.query(`ALTER TABLE Records AUTO_INCREMENT = 1`,(err,result)=>{res.send(err,result)})
 
   
 })})
