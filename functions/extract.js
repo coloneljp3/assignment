ef5b5cac41fdc1983f2aa85aece98240350059c1
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-05-05 04:34:30 -0500

    Update extract.js

--- functions/extract.js
+++ functions/extract.js
@@ -7,17 +7,11 @@ var bodyParser = require('body-parser')
 var serverless = require('serverless-http')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
-router.get("/",(req,res)=>{
-var conn = mysql.createConnection('mysql://avnadmin:AVNS_om8uYVTBL50tPl05R_4@mysql-1e9f0822-jpbreaux225-37e4.h.aivencloud.com:25589/defaultdb?ssl-mode=REQUIRED')
-conn.query(`Create Table Customers(
-id int NOT NULL,
-username VARCHAR(255),
-pasword VARCHAR(255),
-medical_conditions VARCHAR(255),
-prescription_drugs VARCHAR(255),
-PRIMARY KEY (id)
-)`,(err,results)=>{res.send([err,results])})
-})
+router.get('/',(req,res)=>{res.send(`
+ 
+ <form method = "POST" action = "/.netlify/functions/extractdb"><input name = "query" placeholder = "type query"/><button type = "submit"></button></form>
+ 
+ `)})
 
 app.use('/.netlify/functions/extract',router)
 module.exports.handler = serverless(app)
