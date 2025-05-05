3d5391243f9035c9e84fce007eb156ffd546ad47
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-05-05 06:07:15 -0500

    Update spare.js

--- functions/spare.js
+++ functions/spare.js
@@ -9,8 +9,22 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 router.get("/",(req,res)=>{
 var conn = mysql.createConnection('mysql://avnadmin:AVNS_om8uYVTBL50tPl05R_4@mysql-1e9f0822-jpbreaux225-37e4.h.aivencloud.com:25589/defaultdb?ssl-mode=REQUIRED')
-conn.query(`ALTER TABLE Customers AUTO_INCREMENT=1;
-`,(err,result)=>{res.send(err,result)})})
+conn.query(`Create Table Customers(
+id int(16) auto_increment,
+username varchar(255),
+pasword varchar(255),
+medical_conditions TEXT NOT NULL,
+prescription_drugs TEXT NOT NULL,
+PRIMARY KEY (id)
+)
+`,(err,result)=>{
+
+
+
+connect.query(`ALTER TABLE Customers AUTO_INCREMENT = 1`,(err,result)=>{res.send(err,result)})
+
+  
+})})
 
 app.use('/.netlify/functions/spare',router)
 module.exports.handler = serverless(app)
