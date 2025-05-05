9a0e3c95d509bc963101d5a51a15269a3d91852d
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-05-05 04:48:18 -0500

    Update spare.js

--- functions/spare.js
+++ functions/spare.js
@@ -9,7 +9,15 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 router.get("/",(req,res)=>{
 var conn = mysql.createConnection('mysql://avnadmin:AVNS_om8uYVTBL50tPl05R_4@mysql-1e9f0822-jpbreaux225-37e4.h.aivencloud.com:25589/defaultdb?ssl-mode=REQUIRED')
-conn.query(`ALTER TABLE Customers AUTO_INCREMENT=0`,(err,results)=>{res.send([err,results])})
+conn.query(`Create Table Customers(
+id int NOT NULL,
+username varchar(255),
+pasword varchar(255),
+medical_conditions VARCHAR(255),
+prescription_drugs varchar(255),
+PRIMARY KEY (id)
+)`,(err,results)=>{conn.query(`ALTER TABLE Customers AUTO_INCREMENT=0`,(err,results)=>{res.send([err,results])})})
+  
 })
 
 app.use('/.netlify/functions/spare',router)
