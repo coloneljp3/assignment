1b23c1465da8d64f72554bf21518d6412ed1ad47
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-05-05 00:43:24 -0500

    Update signup.js

--- functions/signup.js
+++ functions/signup.js
@@ -17,7 +17,6 @@ router.post('/',(req,res)=>{
 var body = (new String(req.body)).replaceAll('username=','').replaceAll('&pasword=',',').replaceAll('&medical_conditions=',',').replaceAll('&prescription_data',',').split(',')
 var username = body[0]
 var pasword = body[1]
-var connect = mysql.createConnection('mysql://avnadmin:AVNS_om8uYVTBL50tPl05R_4@mysql-1e9f0822-jpbreaux225-37e4.h.aivencloud.com:25589/defaultdb?ssl-mode=REQUIRED')
 
 var medical_conditions = body[2]
 
