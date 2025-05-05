ca280a10e75dd0a1bc35608f85f0af94f3e4bd88
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-05-05 07:14:10 -0500

    Update account-page.js

--- functions/account-page.js
+++ functions/account-page.js
@@ -17,17 +17,15 @@ var pasword = body[1]
 var connect = mysql.createConnection('mysql://avnadmin:AVNS_om8uYVTBL50tPl05R_4@mysql-1e9f0822-jpbreaux225-37e4.h.aivencloud.com:25589/defaultdb?ssl-mode=REQUIRED')
 
 connect.query(`SELECT COUNT(*) FROM Customers where username = ? AND pasword = ?`,[username,pasword],(err,result)=>{
-if(result[0]["COUNT(*)"] == 0){res.send(`This account doesn't exist. You need to create an account first`)}
+if(result[0]["COUNT(*)"] == 0){res.send(``)}
 
 else{
-	connect.query(`Select * from Customers where username = ? and pasword = ?`,[username,pasword],(err,results,fields)=>{
+	connect.query(`Select * from Customers where username = ? and pasword = ?`,[username,pasword],(err,result,fields)=>{
+var customerInfo = result
 var date = new Date();
 var newDate = (date.getDay(),date.getMonth(),date.getYear)
-	connect.query(`Select * from Records where date = ?`,[newDate],(err,result,fields)=>{
-		if(result.length == 0){
-			res.send(`window.alert('You haven't taken your prescriptions yet today. This is a reminder to do so.')`)
-			
-		}
+	connect.query(`Select * from Records where daily_dosage = ? AND username=?`,[newDate,username],(err,result,fields)=>{
+		res.send(result,customerInfo)
 		
 	})
   
