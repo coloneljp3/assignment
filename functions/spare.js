5eeb3f8a208cfa3f13731a4712bbead7320db47c
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-05-05 05:54:40 -0500

    Update spare.js

--- functions/spare.js
+++ functions/spare.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 router.get("/",(req,res)=>{
 var conn = mysql.createConnection('mysql://avnadmin:AVNS_om8uYVTBL50tPl05R_4@mysql-1e9f0822-jpbreaux225-37e4.h.aivencloud.com:25589/defaultdb?ssl-mode=REQUIRED')
-conn.query(`ALTER TABLE Customers AUTO_INCREMENT=0;
+conn.query(`ALTER TABLE Customers AUTO_INCREMENT=1;
 `,(err,result)=>{res.send(err,result)})})
 
 app.use('/.netlify/functions/spare',router)
