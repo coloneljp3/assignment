26ec995757a5d4d2e419183e06536b2464a89185
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-05-07 06:41:49 -0500

    Update form_sub.js

--- functions/form_sub.js
+++ functions/form_sub.js
@@ -1,49 +1,22 @@
-dise = req.body
-var substance_abuse = {start:dise.alc_drug_start,
-                    end:dise.alc_drug_end,
-                    comments:dise.alc_drug_comments  
-                      }
-var asthma = {start:dise.asthma_start,
-                    end:dise.asthma_end,
-                    comments:dise.asthma_comments  
-                      }
-var cancer = {start:dise.cancer_start,
-                    end:dise.cancer_end,
-                    comments:dise.cancer_comments  
-                      }
-var depressives = {start:dise.dep_anx_start,
-                    end:dise.dep_anx_end,
-                    comments:dise.dep_anx_comments  
-                      }
-var diabetes = {start:dise.diab_start,
-                    end:dise.diab_end,
-                    comments:dise.diab_comments  
-                      }
-var copd = {start:dise.copd_start,
-                    end:dise.copd_end,
-                    comments:dise.copd_comments  
-                      }
-var heart_disease = {start:dise.heart_d_start,
-                    end:dise.heart_d_end,
-                    comments:dise.heart_d_comments  
-                      }
-var highbloodpressure = {start:dise.HBP_start,
-                    end:dise.HBP_end,
-                    comments:dise.HBP_comments  
-                      }
-var highcholesterol = {start:dise.hcholesterol_start,
-                    end:dise.hcholesterol_end,
-                    comments:dise.hcholesterol_comments  
-                      }
-var thyroiddisease = {start:dise.thy_d_start,
-                    end:dise.thy_d_end,
-                    comments:dise.thy_d_comments  
-                      }
-var kidneydisease = {start:dise.ren_d_start,
-                    end:dise.ren_d_end,
-                    comments:dise.ren_d_comments  
-                      }
-var migraine = {start:dise.migraine_start,
-                    end:dise.migraine_end,
-                    comments:dise.migraine_comments  
-                      }
+'use strict'
+const mysql = require('mysql2');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const serverless = require('serverless-http');
+const router = express.Router();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+router.post('/',(req,res)=>{
+var body = (new String(req.body)).replaceAll('username=','').replaceAll('&daily_dosage=',',').split(',')
+var username = body[0]
+var daily_dosage = body[1]
+var conn = mysql.createConnection('mysql://avnadmin:AVNS_om8uYVTBL50tPl05R_4@mysql-1e9f0822-jpbreaux225-37e4.h.aivencloud.com:25589/defaultdb?ssl-mode=REQUIRED')
+conn.query(`INSERT INTO Records(username,daily_dosage) VALUES(?,?)`,[username,daily_dosage],(err,result)=>{
+res.send(result)
+  
+}) 
+})
+
+app.use('/.netlify/functions/form_sub',router)
+module.exports.handler = serverless(app)
