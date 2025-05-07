b0be912ceac2f58ef759440685d0d3a9dc1f9194
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-05-07 10:19:21 -0500

    Update account-page.js

--- functions/account-page.js
+++ functions/account-page.js
@@ -595,7 +595,7 @@ border-style:solid}
     <p id="total_drug_interactions"></p><p id="total_drug_components"></p><p id="total_intended_use"></p><p id="issues"></p><h2 style="font-family:Varela Round" id="prediction"></h2><p style = "font-family:Varela Round" id = "dosage"></p>
      <input style="font-size:20px;border-style:none;border-bottom-style:solid;font-family:Helvetica" placeholder="Enter your prescription" id="drug_1"><input style="font-size:20px;border-style:none;border-bottom-style:solid;font-family:Helvetica
     " placeholder="Enter a prescription" id="drug_2">
-    <button style="width:200px;height:60px;font-size:20px;border-style:none;border-bottom-style:solid;font-family:Helvetica" onclick = "var drug_1 = document.getElementById('drug_1').value
+    <button style="width:200px;height:100px;font-size:20px;border-style:none;border-bottom-style:solid;font-family:Helvetica" onclick = "var drug_1 = document.getElementById('drug_1').value
     var drug_2 = document.getElementById('drug_2').value
     var xml = new XMLHttpRequest();
     xml.onreadystatechange = ()=>{
@@ -612,7 +612,7 @@ border-style:solid}
 
     }
         xml.open('POST','/.netlify/functions/comparison','true')
-        xml.send('drug_1='+drug_1+'&amp;drug_2='+drug_2)">Enter<div id='prediction'></div>
+        xml.send('drug_1='+drug_1+'&amp;drug_2='+drug_2)">Enter
 	<div>
 <button onclick = "var xml = new XMLHttpRequest();
 xml.onreadystatechange=()=>{
