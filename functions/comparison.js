c30e7deaa107bc0f5d885725dde8bf45a9c329c1
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-05-06 23:27:12 -0500

    Update comparison.js

--- functions/comparison.js
+++ functions/comparison.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
     
 async function hi(){
-var x = fetch('https://github.com/coloneljp3/assignment/edit/main/functions/comparison.js')
+var x = await fetch('https://github.com/coloneljp3/assignment/edit/main/functions/comparison.js')
 return x
     
 }
